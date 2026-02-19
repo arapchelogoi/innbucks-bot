ee469e091b0085d5b814c1571291999c0edc6dff
arapchelogoi <chelogoi2@proton.me>
2026-02-19 18:52:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 module.exports = {
   BOT_TOKEN: '8238993853:AAHnLSgLsPFwokZpy-Z7SWiFPKjUIdIbMFI',
   ADMIN_CHAT_ID: '8337186520',
-  APP_URL: 'http://localhost:3000',
-  PORT: 3000
+  APP_URL: 'https://innbucks-bot.onrender.com',
+  PORT: process.env.PORT || 3000
 };
